35276686e089163fe72032cb3c4f158279f7e67e
João Victor da Silva <jv782063@gmail.com>
2021-04-20 17:04:44 -0300

    makes a call to a fake API and generates the page statically

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
   return <h1>Podcastr</h1>;
+}
+
+/* SSR
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+  };
+}
+*/
+
+// SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // a cada 8 horas uma chamada
+  };
 }
